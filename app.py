959dc6edf37304955d8c16dc731f8d9b2e11d7cd
Rahul Bunker <rahulbunker6255@gmail.com>
2026-05-17 00:28:16 +0530

    Update model path to churn_rf_v1_optuna.pkl

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 import os
 
-MODEL_PATH = "churn_rf_simple.pkl"
+MODEL_PATH = "churn_rf_v1_optuna.pkl"
 model = scaler = ohe = num_cols = cat_cols = None
 model_loaded = False
 
